a083a8e6adc4429f96c54afa4c6ad90d96005d6f
Navpreet Kaur <navpreet.kaur01@nagarro.com>
2018-05-17 14:55:57 +0530

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = express_1.default();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({
+    extended: true
+}));
+var port = process.env.PORT || 7080;
+app.use('/', express_1.default.static(path_1.default.join(__dirname, 'dist')));
+app.listen(port, () => console.log('Server started at http://localhost:' + port));
